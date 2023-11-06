a1ead4ad5e9a6e890fb51528ddb718b333aad8bd
LuisRz1 <langulor1@upao.edu.pe>
2023-11-06 15:40:59 -0500

    first commit

---
+++ src/main/java/edu/upa/pe/iloveltravelbackend/ILovelTravelBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.upa.pe.iloveltravelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILovelTravelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILovelTravelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/upa/pe/iloveltravelbackend/ILovelTravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upa.pe.iloveltravelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILovelTravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
